8bc3a68834adbf1097e28a40a4febac475bd9dae
FanciGoD <minhbvht@gmail.com>
2025-02-04 20:54:20 +0700

    updated

--- src/main/java/com/adeptus/management/entity/Student.java
+++ src/main/java/com/adeptus/management/entity/Student.java
@@ -36,9 +36,6 @@ public class Student extends BaseEntity {
     @Column(nullable = false)
     private LocalDate dob;  // Ngày sinh
 
-    @Column(nullable = false)
-    private Integer lessonRemain;  // Số buổi học còn lại
-
     @OneToMany(mappedBy = "student")
     private Set<StudentClass> studentClasses; // Quan hệ 1:N với StudentClass
 
