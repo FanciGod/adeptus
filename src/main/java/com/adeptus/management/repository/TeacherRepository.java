61e03af554fe12d1fde0020045e44fe20845e1df
sonDew <v.son8697@gmail.com>
2025-02-14 22:01:35 +0700

    Update teacher Response (include salaries and classes)

--- src/main/java/com/adeptus/management/repository/TeacherRepository.java
+++ src/main/java/com/adeptus/management/repository/TeacherRepository.java
@@ -13,14 +13,10 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
     @Query("SELECT t FROM Teacher t LEFT JOIN FETCH t.salaries LEFT JOIN FETCH t.classes WHERE t.isActive = true")
     List<Teacher> getAllActiveTeachers();
-//    @Query("SELECT t FROM Teacher t LEFT JOIN FETCH t.classes c WHERE t.isActive = true")
-//    List<Teacher> getAllActiveTeachers();
 
     @Query("SELECT t FROM Teacher t LEFT JOIN FETCH t.salaries LEFT JOIN FETCH t.classes WHERE t.isActive = true")
     Page<Teacher> getAllActiveTeachers(Pageable pageable);
 
     @Query("SELECT t FROM Teacher t LEFT JOIN FETCH t.salaries LEFT JOIN FETCH t.classes WHERE t.isActive = true AND t.id = ?1")
     Optional<Teacher> findActiveTeacherById(Long id);
-//    @Query("SELECT t FROM Teacher t LEFT JOIN FETCH t.classes c WHERE t.isActive = true AND t.id = ?1")
-//    Optional<Teacher> findActiveTeacherById(Long id);
 }
